2558206da9a9613c056aee3f9578853c7a09d34c
Brenda Escoto <escot105@mail.chapman.edu>
2019-11-29 13:54:56 -0800

    added GUI elements to client

--- battleClient.java
+++ battleClient.java
@@ -8,6 +8,10 @@ import java.net.Socket;
 
 import java.util.Scanner;
 
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.text.JTextComponent;
 
 public class battleClient {
 
@@ -25,7 +29,9 @@ public class battleClient {
 
         System.out.println("Let's go ladies.");
 
-        Listener l = new Listener(cs);
+
+        Board t1 = new Board();
+        Listener l = new Listener(cs,t1);
         Thread t = new Thread(l);
         t.start();
         String data;
@@ -52,12 +58,120 @@ public class battleClient {
     }
 }
 
+
+class Board
+{
+  //variables from GUI
+  JFrame f;//the window
+  JLabel bg,instructions;
+  JTextArea errormsg;
+
+  ImageIcon miss;//no ship was hit
+  ImageIcon hit; //a ship was hit
+
+  //user and enemy boards
+  JLabel uScreen[] = new JLabel[100];
+  JLabel eScreen[] = new JLabel[100];
+
+  //title image
+  ImageIcon t;
+  JLabel title;
+
+  int x, y;
+  Boolean newLine;
+
+  Board()
+  {
+    f=new JFrame("Battleship");
+    bg = new JLabel(new ImageIcon("board.png"));
+    bg.setVisible(true);//set false
+
+    t = new ImageIcon("title.png");
+    title = new JLabel(t);
+    title.setBounds(0,0,1090,60);
+    f.add(title);
+
+    errormsg = new JTextArea("Welcome to the game.",3,20);
+    errormsg.setBounds(30,600,500,100);
+    errormsg.setFont(new Font("Helvetica Neue", Font.PLAIN, 14));
+    errormsg.setEditable(false);
+    errormsg.setVisible(true);
+    f.add(errormsg);
+
+    //for loop to set explosions up
+    miss = new ImageIcon("lol.png");
+    hit = new ImageIcon("sad.png");
+
+    x = 35; //x coordinate of first box
+    y = 95; //y coordinate of first box
+    int z = 40; //size of image
+    int q=0;
+    for(int i = 0;i<100;i++)
+    {
+
+      uScreen[i] = new JLabel(miss);
+      eScreen[i] = new JLabel(miss);
+
+      uScreen[i].setBounds(x,y,z,z);
+      eScreen[i].setBounds(x+515,y,z,z);
+
+      f.add(uScreen[i]);
+      f.add(eScreen[i]);
+
+      uScreen[i].setVisible(true);
+      eScreen[i].setVisible(true);
+
+      x = x + 51;
+      q++;
+
+      if(q==10) // new row
+      {
+        //System.out.print(i);
+        x = 35;
+        y = y + 50;
+        q = 0;
+      }
+    }
+
+    newLine= true;
+
+    f.add(bg);
+    f.setSize(1100,700);
+    f.setVisible(true);
+    f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+  }
+
+  public void setMessage(String s)
+  {
+
+    if(newLine)
+    {
+      String temp = errormsg.getText();
+      errormsg.setText(temp + "\n" + s);
+      newLine=false;
+    }
+    else
+    {
+      errormsg.setText(s);
+      newLine= true;
+    }
+  }
+
+
+
+}
+
+
 class Listener implements Runnable {
   private Socket cs = null;
+  Board b;
 
-  Listener(Socket q)
+  Listener(Socket q, Board t)
   {
     cs = q;
+    b = t;
   }
 
   public void run()
@@ -71,8 +185,9 @@ class Listener implements Runnable {
         String serverText = fromServer.readLine();
         if (fromServer != null)
         {
+          b.setMessage(serverText);
           System.out.println(serverText);
-          
+
         }
         else
         {
